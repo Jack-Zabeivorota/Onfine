7726e5f6d1a470922668e751ab1a415bb250bd96
Jack <jeck.zabeivorota@gmail.com>
2024-08-03 11:39:34 +0300

    optimized sending user status

--- Onfine backend/main.py
+++ Onfine backend/main.py
@@ -3,10 +3,12 @@ from fastapi import FastAPI, WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
+from sqlalchemy import and_, select
+from sqlalchemy.orm import aliased
 import models.requests as reque
 import models.responses as resp
 from database.database import SessionMaker
-from database.models import User 
+from database.models import User, ChatUser, Chat, ChatTypes
 from tools import check_auth, parse_model, json_response, connected_clients, get_now_datetime, logout_user
 
 from routes.chat import router as chat_router
@@ -37,10 +39,28 @@ def view():
 
 
 async def send_user_status(user_id: str, is_online: bool):
+    with SessionMaker() as sess:
+        chatUser1: ChatUser = aliased(ChatUser)
+        chatUser2: ChatUser = aliased(ChatUser)
+
+        query = select(chatUser2.user_id).where(
+            Chat.type == ChatTypes.chat.name
+        ).join(
+            chatUser1, and_(
+            chatUser1.chat_id == Chat.id,
+            chatUser1.user_id == user_id
+        )).join(
+            chatUser2, and_(
+            chatUser2.chat_id == Chat.id,
+            chatUser2.user_id != user_id
+        ))
+        user_ids: list[str] = sess.execute(query).scalars().all()
+
     notifi = resp.NewStatus(user_id=user_id, is_online=is_online).model_dump_json()
 
-    for socket in connected_clients.values():
-        await socket.send_text(notifi)
+    for id_ in user_ids:
+        if id_ in connected_clients:
+            await connected_clients[id_].send_text(notifi)
 
 
 @app.websocket('/ws')
@@ -79,4 +99,4 @@ async def ws(socket: WebSocket):
 
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True)
+    uvicorn.run('main:app', host='0.0.0.0', port=8000)
